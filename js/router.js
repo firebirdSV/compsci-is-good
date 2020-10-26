a0a0ac14af85fd05725cc5b44a93ff5843fb746b
Tom Abbott <=>
2020-10-26 12:39:48 +0000

    Update article loading process

--- js/router.js
+++ js/router.js
@@ -1,10 +1,24 @@
 // Renders the home screen
 function loadHomePage() {
-    console.log("loaded home page");
+    mainTag = document.getElementById("main");
+
+    fetch("home.html")
+        .then(HTMLcode => HTMLcode.text())
+        .then(homepage => mainTag.innerHTML = homepage);
 }
 
-function loadArticle(articeName) {
-    console.log("loading article ", articeName);
+/* loadArticle injects the HTML of a specified article
+   in to the `main` tag of index.html.
+ */
+function loadArticle(articleName) {
+    // Reference to the tag we're updating
+    mainTag = document.querySelector("main");
+    
+    // Load the article as HTML code. Then convert it to a
+    // string. Finally, we insert the string in to `index.html` 
+    fetch(`articles/${articleName}.html`)
+        .then(HTMLcode => HTMLcode.text())
+        .then(article => mainTag.innerHTML = article);
 }
 
 // Initialise the webpage
