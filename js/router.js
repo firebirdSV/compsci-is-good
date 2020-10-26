02fa1456eae1ed49cd83c08e7aa7a993383dfe01
Tom Abbott <=>
2020-10-26 11:56:01 +0000

    Inital commit for JS code

---
+++ js/router.js
@@ -0,0 +1,13 @@
+// Renders the home screen
+function loadHomePage() {
+    console.log("loaded home page");
+}
+
+function loadArticle(articeName) {
+    console.log("loading article ", articeName);
+}
+
+// Initialise the webpage
+window.onload = function() {
+    loadHomePage();
+}
