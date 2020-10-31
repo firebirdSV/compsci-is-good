5f94b884651d7c763fa08d127dcbc40eee4fc05c
Joe Clinton <joeclinton1@btinternet.com>
2020-10-31 23:16:49 +0000

    Added carousel and dynamic content insertion

--- js/index.js
+++ js/index.js
@@ -1,7 +1,101 @@
+function set_card_text(i){
+    var [article_name, article_data] = Object.entries(articles)[i]
+    $('#article-cards .card-title').text(article_name)
+    $('#article-cards .card-text').text(article_data['description'])
+}
+
+function set_article_text(article_name){
+    $('article').show()
+    $('article .card-title').text(article_name)
+    loadArticle(article_name);
+}
+
+
+Number.prototype.mod = function(n) {
+    return ((this%n)+n)%n;
+};
+
+var articles
+$.getJSON('articles/articles.json', function(data){
+    articles = data["articles"]
+});
+   
 $(document).ready(function(){
+
     //Add active to nav-link on click, remove if click on another
-    $(".navbar .nav-link").on("click", function(){
+
+    var i = 0;
+    for(var article_name in articles){
+        var article_data = articles[article_name]
+        $('#alt-navbar div.card-group').append(`
+        <div class = 'article-img-container'>
+            <img class = 'article-img' src = 'images/${article_data['image']}'>
+        </div>
+        `)
+
+        $('.nav-pages').append(`
+        <li class="nav-item"><a class = 'nav-link' href="#about_us">${article_name}</a></li>
+        `)
+
+        if(i==0){
+            set_card_text(i)
+        }
+
+        var active = (i==0 ? "active" : '')
+        $(".carousel-inner").prepend(`
+            <div class="carousel-item ${active}">
+                <img class="d-block" src= 'images/${article_data['image']}' alt="">
+            </div>
+        `)
+
+        $('.carousel-indicators').append(`
+            <li data-target="#CarouselTest" data-slide-to="${i}" class="${active}"></li>
+        `)
+        i++;
+    }
+
+    //hide article until 'Read More' is clicked for the frst time
+    $('article').hide()
+
+    i = 0
+
+    //add js for carousel
+    $(".carousel-control-next").on("click", ()=>{
+        i= (i+1).mod(Object.keys(articles).length)
+        set_card_text(i)
+    });
+
+    $(".carousel-control-prev").on("click", ()=>{
+        i = (i-1).mod(Object.keys(articles).length)
+        set_card_text(i)
+    });
+
+    $(".carousel-indicators>li").click(function(){
+        console.log($(this))
+        i = parseInt($(this).attr('data-slide-to'))
+        set_card_text(i)
+    })
+
+    //inject article when read_more btn clicked
+    $("#article-cards .btn").on("click", ()=>{
+        var article_name = Object.keys(articles)[i]
+        set_article_text(article_name)
+    })
+
+    //Set navlink to active when clicked
+    $(".nav-link").click(function(){
         $(".navbar").find(".active").removeClass("active");
         $(this).addClass("active");
-     });
+    });
+
+    //inject article when navbar btn clicked
+    $(".nav-pages .nav-link").click(function(){
+        $(".navbar").find(".active").removeClass("active");
+        $(this).addClass("active");
+
+        var article_name = $(this).text()
+        set_article_text(article_name)
+        i = Object.keys(articles).indexOf(article_name)
+        set_card_text(i)
+    })
 });
--- js/router.js
+++ js/router.js
@@ -5,45 +5,21 @@ to view a page. This creates the effct of visiting different webpages, but the
 user stays on `index.html`. That's why `index.html` looks empty. 
 
 For example, if the user were to click on the "social media" button in the navbar,
-the website would use Javascript to replace the `innerHTML` of the `main` tag in 
+the website would use Javascript to replace the `innerHTML` of the element with id `article-body` in 
 `index.html` .
  */
 
-// Renders the home screen
-function loadHomePage() {
-    mainTag = document.querySelector("main");
-
-    fetch("articles/home.html")
-        .then(HTMLcode => HTMLcode.text())
-        .then(homepage => mainTag.innerHTML = homepage);
-}
 
 /* loadArticle injects the HTML of a specified article
    in to the `main` tag of index.html.
  */
 function loadArticle(articleName) {
     // Reference to the tag we're updating
-    mainTag = document.querySelector("main");
-    
+    articleBody = document.getElementById('article-body');
+    articleName = articleName.replace(/\s+/g, '-').toLowerCase()
     // Load the article as HTML code. Then convert it to a
     // string. Finally, we insert the string in to `index.html` 
     fetch(`articles/${articleName}.html`)
         .then(HTMLcode => HTMLcode.text())
-        .then(article => mainTag.innerHTML = article);
-}
-
-// Initialise the webpage
-window.onload = function() {
-
-    // Make navbar buttons functional
-    // Would be good to cut out this whole code block and iterate over nav bar elements
-    // from `document.getElementByClassName("nav-item")` and set the behaviour that way.
-    document.getElementById("home").onclick = () => loadHomePage();
-    document.getElementById("about-us").onclick = () => loadArticle("about-us");
-    document.getElementById("education").onclick = () => loadArticle("education");
-    document.getElementById("flood-prediction").onclick = () => loadArticle("flood-prediction");
-    document.getElementById("medicine").onclick = () => loadArticle("medicine");
-    document.getElementById("social-media").onclick = () => loadArticle("social-media");
-    
-    loadHomePage();
+        .then(article => articleBody.innerHTML = article);
 }
