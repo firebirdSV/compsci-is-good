fef8db274bd6599fadf159b023bc4d1da3d9cd85
Joe Clinton <joeclinton1@btinternet.com>
2020-11-01 03:05:10 +0000

    Add pictures to about_us

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-function set_card_text(i){
+function set_card_text(i) {
     var [article_name, article_data] = Object.entries(articles)[i]
     $('#article-cards .card-title').text(article_name)
     $('#article-cards .card-text').text(article_data['description'])
 }
 
-function set_article_text(article_name){
+function set_article_text(article_name) {
     $('article').show()
     $('article .card-title').text(article_name)
     loadArticle(article_name);
 }
 
-function insert_author_card(author_name){
+function insert_author_card(author_name) {
     var author = authors[author_name]
     $("#about-us-container").append(`
         <div class="card">
@@ -20,45 +20,47 @@ function insert_author_card(author_name){
                         <h4 class="card-title">${author_name}</h4>
                         <p class="card-text">${author["bio"]}</p>
                         <br>
-                        <a href="#article-body" class="mt-auto btn btn-primary  ">Linked in</a>
-                        <a href="#article-body" class="mt-auto btn btn-primary  ">Github</a>
+                        <a href="${author['linked-in']}" class="mt-auto btn btn-primary  ">Linked in</a>
+                        <a href="${author['github']}" class="mt-auto btn btn-primary  ">Github</a>
                     </div>
                 </div>
-                <div class="col-md-5"><img class = 'author_img' src = 'authors/images/${author["image"]}'></div>
+                <div class="col-md-5 p-5 author-img-container">
+                    <img class = 'author-img img-responsive rounded-circle' data-holder-rendered='true' alt='profile picture' src = 'authors/images/${author["image"]}'>
+                </div>
             </div>
         </div>
     `)
 }
 
-Number.prototype.mod = function(n) {
-    return ((this%n)+n)%n;
+Number.prototype.mod = function (n) {
+    return ((this % n) + n) % n;
 };
 
 var articles
-$.getJSON('articles/articles.json', function(data){
+$.getJSON('articles/articles.json', function (data) {
     articles = data["articles"]
 });
 
 var authors
-$.getJSON('authors/authors.json', function(data){
+$.getJSON('authors/authors.json', function (data) {
     authors = data["authors"]
 });
 
-   
-$(document).ready(function(){
+
+$(document).ready(function () {
 
     //Add active to nav-link on click, remove if click on another
 
     var i = 0;
-    for(var article_name in articles){
+    for (var article_name in articles) {
         var article_data = articles[article_name]
-        var active = (i==0 ? "active" : '')
+        var active = (i == 0 ? "active" : '')
 
         $('.nav-pages').append(`
         <li class="nav-item"><a class = 'nav-link ${active}' href="#about_us">${article_name}</a></li>
         `)
 
-        if(i==0){
+        if (i == 0) {
             set_card_text(i)
         }
 
@@ -75,7 +77,7 @@ $(document).ready(function(){
         i++;
     }
 
-    for(var author_name in authors){
+    for (var author_name in authors) {
         insert_author_card(author_name)
     }
 
@@ -86,36 +88,36 @@ $(document).ready(function(){
     i = 0
 
     //add js for carousel
-    $(".carousel-control-next").on("click", ()=>{
-        i= (i+1).mod(Object.keys(articles).length)
+    $(".carousel-control-next").on("click", () => {
+        i = (i + 1).mod(Object.keys(articles).length)
         set_card_text(i)
     });
 
-    $(".carousel-control-prev").on("click", ()=>{
-        i = (i-1).mod(Object.keys(articles).length)
+    $(".carousel-control-prev").on("click", () => {
+        i = (i - 1).mod(Object.keys(articles).length)
         set_card_text(i)
     });
 
-    $(".carousel-indicators>li").click(function(){
+    $(".carousel-indicators>li").click(function () {
         console.log($(this))
         i = parseInt($(this).attr('data-slide-to'))
         set_card_text(i)
     })
 
     //inject article when read_more btn clicked
-    $("#article-cards .btn").on("click", ()=>{
+    $("#article-cards .btn").on("click", () => {
         var article_name = Object.keys(articles)[i]
         set_article_text(article_name)
     })
 
     //Set navlink to active when clicked
-    $(".nav-link").click(function(){
+    $(".nav-link").click(function () {
         $(".navbar").find(".active").removeClass("active");
         $(this).addClass("active");
     });
 
     //inject article when navbar btn clicked
-    $(".nav-pages .nav-link").click(function(){
+    $(".nav-pages .nav-link").click(function () {
         $("#article-cards").show()
         $("#article-container").show()
         $("#about-us-container").hide()
@@ -126,10 +128,9 @@ $(document).ready(function(){
         set_card_text(i)
     })
 
-    $(".nav-about-us").click(function(){
+    $(".nav-about-us").click(function () {
         $("#article-cards").hide()
         $("#article-container").hide()
         $("#about-us-container").show()
-
     })
 });
